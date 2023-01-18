879f05c8d19bb0e3931d1790f585e8696736877d
valentin-panov <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>
2023-01-18 18:22:42 +0100

    arch in progress

---
+++ components/layouts/main/mainLayout.tsx
@@ -0,0 +1,32 @@
+import styles from "../../../styles/Home.module.css";
+import { Footer, Header, Main, Menu } from "../../../components";
+import React, { ReactNode } from "react";
+import Head from "next/head";
+
+interface MainProps {
+  children: ReactNode | ReactNode[];
+  keywords?: string;
+}
+
+const MainLayout: React.FC<MainProps> = ({ children, keywords }) => {
+  return (
+    <>
+      <Head>
+        <meta
+          name="keywords"
+          content={"vulnerable, nextjs, security" + keywords && `, ${keywords}`}
+        />
+        <meta name={"author"} content={"Valentin Panov"} />
+        <title>Vulnerable NextJS App</title>
+      </Head>
+      <div className={styles.container}>
+        <Header />
+        <Menu />
+        <Main>{children}</Main>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
