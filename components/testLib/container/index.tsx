e799a5543d82c840121c9fbc4816d7869ab72159
Valentin Panov <valentin.panov@me.com>
2023-03-24 23:06:39 +0100

    feat(components/testLib/poc_JSONparse/JSONparse.tsx) RCE added

--- components/testLib/container/index.tsx
+++ components/testLib/container/index.tsx
@@ -9,39 +9,42 @@ import {
   ExampleRouter,
   FormAction,
   HtmlAttr,
+  JSONparse,
   LocationHref,
-  NextRedirect, PopulatedInput,
+  NextRedirect,
+  PopulatedInput,
   RefInnerHTML,
-  WindowOpen
+  WindowOpen,
 } from "@components/testLib";
 import Barrel from "@components/testLib/container/barrel";
 
 const components: {
   [propName: string]: React.FunctionComponent<{ payload: string }>;
 } = {
-  dangerouslySetInnerHTML: DSIH,
   next_router_push: ExampleRouter,
   form_action: FormAction,
   location_href: LocationHref,
   next_redirect: NextRedirect,
-  ref_InnerHTML: RefInnerHTML,
   window_open: WindowOpen,
+  dangerouslySetInnerHTML: DSIH,
+  ref_InnerHTML: RefInnerHTML,
+  json_parse_RCE: JSONparse,
   SAFE_classNames: ClassN,
   SAFE_data_Test_Id: DataTestId,
   SAFE_html_Attr: HtmlAttr,
   SAFE_dangerouslySetInnerHTML_script: DSIHscript,
-  SAFE_input: PopulatedInput
+  SAFE_input: PopulatedInput,
 };
 
 const examples: string[] = [
-  `<img src="xxx:x" onerror=alert('pwnd')>`,
   `javascript:alert(1)`,
   `javascript:alert(document.cookie.split(';'))`,
+  `<img src="xxx:x" onerror=alert('pwnd')>`,
 ];
 
 const TestContainer: React.FC = () => {
-  const [inputString, setInputString] = React.useState("javascript:alert(1)");
-  const [element, setElement] = React.useState(Object.keys(components)[1]);
+  const [inputString, setInputString] = React.useState(examples[0]);
+  const [element, setElement] = React.useState(Object.keys(components)[0]);
 
   const onChangeInputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputString(event.target.value);
@@ -55,14 +58,14 @@ const TestContainer: React.FC = () => {
             value={inputString}
             onChangeHandler={onChangeInputHandler}
           />
-          <p>
-            Examples:
-            <ul>
-              {examples.map((el) => (
-                <li key={el}>{el}</li>
-              ))}
-            </ul>
-          </p>
+          <p>Examples:</p>
+          <ul>
+            {examples.map((el) => (
+              <li key={el}>
+                <p>{el}</p>
+              </li>
+            ))}
+          </ul>
           <br />
           <label htmlFor={"element_select"}>Tested element: </label>
           <select
---
+++ components/testLib/poc_JSONparse/JSONparse.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import styles from "./JSONparse.module.scss";
+
+interface Props {
+  payload: string;
+}
+
+const JSONparse: React.FC<Props> = ({ payload }) => {
+  const deepCopy = (obj: unknown) => {
+    if (!obj) return obj;
+
+    const copied: Record<string, unknown> = {};
+
+    for (const [key, value] of Object.entries(obj as unknown as object)) {
+      switch (typeof value) {
+        case "number":
+        case "symbol":
+        case "boolean":
+        case "undefined":
+          copied[key] = value;
+          break;
+        case "string":
+          if (value.substr(0, 8) == "function") {
+            const startBody = value.indexOf("{") + 1;
+            const endBody = value.lastIndexOf("}");
+            const startArgs = value.indexOf("(") + 1;
+            const endArgs = value.indexOf(")");
+
+            copied[key] = new Function(
+              value.substring(startArgs, endArgs),
+              value.substring(startBody, endBody)
+            );
+          } else {
+            copied[key] = value;
+          }
+          break;
+        case "object":
+          copied[key] = deepCopy(value);
+      }
+    }
+    return copied;
+  };
+
+  const crafted = `{"__proto__":{"toJSON":"function(){${payload};return 1;};"},"foo":"bar"}`;
+  const parsed = JSON.parse(crafted);
+  const copied = deepCopy(parsed);
+  JSON.stringify(copied); // JSON.stringify calls toJSON method under the hood
+
+  return (
+    <>
+      <h2>Insecure deserialization for:</h2>
+      <div className={styles.code}>{crafted}</div>
+    </>
+  );
+};
+
+export default JSONparse;
