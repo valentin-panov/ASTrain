258c131f1efc026155caba6e62ba60a3f56486d3
Valentin Panov <valentin.panov@me.com>
2023-04-02 21:57:45 +0200

    fix(components/testLib/poc_createElement/createElement.tsx) small fixes

--- components/testLib/poc_createElement/createElement.tsx
+++ components/testLib/poc_createElement/createElement.tsx
@@ -2,14 +2,10 @@ import React from "react";
 
 interface Props {
   payload: string;
-  // children: React.ReactNode | React.ReactNode[];
-  // props: Record<string, unknown>;
 }
 
 const CreateElement: React.FC<Props> = ({ payload }) => {
   const [type, propsPayload, childrenPayload] = payload.split("|");
-  // const type = payload;
-  // const type = "scRipt";
   const props = { className: propsPayload };
   const children: React.ReactNode = childrenPayload;
   return React.createElement(type, props, children);
